f3751ddf422b9d069bedf65583980ebdbe066f40
Praveen Chandra Thapliyal <popdev781@gmail.com>
2017-04-16 23:53:19 +0530

    https://www.youtube.com/watch?v=gnsO8-xJ8rs at 20:00

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+/*
+var logger = function(req, res, next) {
+    console.log('Logging...');
+    next();
+}
+app.use(logger);
+*/
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.send('hey user');
+});
+app.listen(3000, function() {
+    console.log('server started on port 3000');
+})
+
+//console.log('hey');
